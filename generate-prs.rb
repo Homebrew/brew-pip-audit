11aca5539ba3e185f4f776d1a18ccad1921859fb
William Woodruff <william@yossarian.net>
2023-02-06 13:05:57 +0100

    generate-prs: hackety hack
    Signed-off-by: William Woodruff <william@yossarian.net>

--- generate-prs.rb
+++ generate-prs.rb
@@ -18,7 +18,7 @@ for path in Dir.entries("audits")
   # TODO: Updating Python resources automatically can fail for myriad reasons;
   # we should try and handle some of them.
   PyPI.update_python_resources!(formula,
-                                ignore_non_pypi_packages: true) rescue next
+                                ignore_non_pypi_packages: true) rescue SystemExit next
 
   args = OpenStruct.new(force?: false, quiet?: false)
   GitHub.check_for_duplicate_pull_requests(formula.name, formula.tap.remote_repo,
