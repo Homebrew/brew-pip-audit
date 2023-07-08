0c6c0a9051a39433febd4a8e6c8908535a8f9be7
Alex Gaynor <alex.gaynor@gmail.com>
2023-07-08 12:28:34 -0400

    Corrected which formula not to upgrade
    Added a comment

--- generate-prs.rb
+++ generate-prs.rb
@@ -14,7 +14,8 @@ SKIP_FORMULA = [
   # https://opendev.org/jjb/jenkins-job-builder/src/branch/master/requirements.txt#L4
   "jenkins-job-builder",
   # PRs don't work due to a bug where we use pre-release dependencies
-  "androguard",
+  # https://gitlab.com/fdroid/fdroidserver/-/merge_requests/1379
+  "fdroidserver",
   # Has a weird PyInstaller-based install that `pip` can't handle
   # https://github.com/Homebrew/homebrew-core/blob/5eb7ab4f78c012514871011fd1ab80fb0911809f/Formula/gyb.rb#L151-L160
   "gyb",
